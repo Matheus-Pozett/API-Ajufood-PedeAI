bcdd10161d643803d347e801ced6d42c7ed47a61
Matheus Pozett <maa_souza11@hotmail.com>
2026-05-06 12:30:12 -0300

    repository CategoriaProduto

---
+++ src/main/java/br/com/ajufood/pedeai/repository/CategoriaProdutoRepository.java
@@ -0,0 +1,9 @@
+package br.com.ajufood.pedeai.repository;
+
+import br.com.ajufood.pedeai.model.CategoriaProdutoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaProdutoRepository extends JpaRepository<CategoriaProdutoModel, Integer> {
+}
