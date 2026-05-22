ca376dc8ba0684b50708d16bdbf8a9499748a4b2
Matheus Pozett <maa_souza11@hotmail.com>
2026-05-22 10:08:00 -0300

    feat: Implementa em ClienteRepository metodos existsByEmail, existsByCpf.

--- src/main/java/br/com/ajufood/pedeai/repository/ClienteRepository.java
+++ src/main/java/br/com/ajufood/pedeai/repository/ClienteRepository.java
@@ -4,6 +4,12 @@ import br.com.ajufood.pedeai.model.ClienteModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ClienteRepository extends JpaRepository<ClienteModel, Integer> {
+    boolean existsByEmail(String email);
+    boolean existsByCpf(String cpf);
+    Optional<ClienteModel> findByCpf(String cpf);
+    Optional<ClienteModel> findByEmail(String email);
 }
