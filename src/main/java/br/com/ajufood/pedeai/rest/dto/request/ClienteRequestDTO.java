0df2aea9cbd2f5b8a21b110d96025a1d84881c8b
Matheus Pozett <maa_souza11@hotmail.com>
2026-05-22 10:13:07 -0300

    feat: Cria dtos de Request e Response para Cliente

---
+++ src/main/java/br/com/ajufood/pedeai/rest/dto/request/ClienteRequestDTO.java
@@ -0,0 +1,35 @@
+package br.com.ajufood.pedeai.rest.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.br.CPF;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClienteRequestDTO {
+    @Length(min = 2, max = 128, message = "O nome deverá ter no mínimo 2 caracteres e no máximo 256 caracteres")
+    @NotBlank(message = "O nome é obrigatório.")
+    private String nome;
+
+    @CPF(message = "CPF inválido!")
+    @Length(min = 11, max = 11, message = "O cpf deverá ter obrigatoriamente 11 digitos")
+    @NotBlank(message = "O CPF é obrigatório.")
+    private String cpf;
+
+
+    @Email(message = "E-mail inválido!")
+    @Length(min = 3, max = 256, message = "O email deverá ter no mínimo 3 caracteres e no máximo 256 caracteres")
+    @NotBlank(message = "O email é obrigatório.")
+    private String email;
+
+    @Length(min = 11, max = 11, message = "O telefone deverá ter obrigatoriamente 11 digitos")
+    @NotBlank(message = "O telefone é obrigatório.")
+    private String telefone;
+}
---
+++ src/main/java/br/com/ajufood/pedeai/rest/dto/response/ClienteResponseDTO.java
@@ -0,0 +1,18 @@
+package br.com.ajufood.pedeai.rest.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClienteResponseDTO {
+    private int id;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String telefone;
+}
