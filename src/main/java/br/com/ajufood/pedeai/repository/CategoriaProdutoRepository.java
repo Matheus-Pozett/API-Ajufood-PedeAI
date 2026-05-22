f5ffa7c6253fa0012fedcfe54c9cccba7c02a0b1
Matheus Pozett <maa_souza11@hotmail.com>
2026-05-22 10:08:40 -0300

    feat: Implementa em CategoriaProdutoRepository metodo existsByNome.

--- src/main/java/br/com/ajufood/pedeai/repository/CategoriaProdutoRepository.java
+++ src/main/java/br/com/ajufood/pedeai/repository/CategoriaProdutoRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoriaProdutoRepository extends JpaRepository<CategoriaProdutoModel, Integer> {
+
+  boolean existsByNome(String nome);
+
 }
