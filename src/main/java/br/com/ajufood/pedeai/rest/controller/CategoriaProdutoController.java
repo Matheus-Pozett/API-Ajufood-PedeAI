2f06e9d7872b9aa2eed0a00d0ffbefa83de30001
Matheus Pozett <maa_souza11@hotmail.com>
2026-05-23 11:23:50 -0300

    refactor: Ajusta paths dos controllers para padrão rest no plural

--- src/main/java/br/com/ajufood/pedeai/rest/controller/CategoriaProdutoController.java
+++ src/main/java/br/com/ajufood/pedeai/rest/controller/CategoriaProdutoController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/categoria")
+@RequestMapping("/categorias")
 @RequiredArgsConstructor // Cria o construtor automaticamente com as variáveis 'final'
 public class CategoriaProdutoController {
 
--- src/main/java/br/com/ajufood/pedeai/rest/controller/ClienteController.java
+++ src/main/java/br/com/ajufood/pedeai/rest/controller/ClienteController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("cliente")
+@RequestMapping("clientes")
 @RequiredArgsConstructor // Cria o construtor automaticamente com as variáveis 'final'
 public class ClienteController {
 
