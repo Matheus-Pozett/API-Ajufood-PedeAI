bdbcd8bff81844e28eb795423df3ad91f08ca1e1
Matheus Pozett <maa_souza11@hotmail.com>
2026-05-30 09:14:09 -0300

    feat: cria lista de pagamentos do relacionamento com PagamentoModel e metodos helpers de add e remove

--- src/main/java/br/com/ajufood/pedeai/model/FormaPagamentoModel.java
+++ src/main/java/br/com/ajufood/pedeai/model/FormaPagamentoModel.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -12,6 +13,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "formaPagamento")
@@ -33,4 +37,17 @@ public class FormaPagamentoModel {
   @Column(name = "descricao", length = 256)
   @Length(min = 10, max = 256, message = "A descrição deverá ter no mínimo 10 caracteres e no máximo 256 caracteres")
   private String descricao;
+
+  @OneToMany(mappedBy = "formaPagamento")
+  private List<PagamentoModel> pagamentos = new ArrayList<>();
+
+  public void addPagamento(PagamentoModel pagamento) {
+    pagamentos.add(pagamento);
+    pagamento.setFormaPagamento(this);
+  }
+
+  public void removePagamento(PagamentoModel pagamento) {
+    pagamentos.remove(pagamento);
+    pagamento.setFormaPagamento(null);
+  }
 }
