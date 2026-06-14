ee32382be5a15d653762756a43524841099c8dae
Matheus Pozett <maa_souza11@hotmail.com>
2026-06-13 23:37:03 -0300

    feat: implementa no repository de produto busca por disponivel e por categoria

--- src/main/java/br/com/ajufood/pedeai/repository/ProdutoRepository.java
+++ src/main/java/br/com/ajufood/pedeai/repository/ProdutoRepository.java
@@ -4,9 +4,15 @@ import br.com.ajufood.pedeai.model.ProdutoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProdutoRepository extends JpaRepository<ProdutoModel, Integer> {
   boolean existsByNome(String nome);
-
   boolean existsByNomeAndIdNot(String name, Integer id);
+
+  List<ProdutoModel> findByDisponivelTrue();
+
+  List<ProdutoModel> findByCategoriaProdutoIdAndDisponivelTrue(Long categoriaId);
+
 }
