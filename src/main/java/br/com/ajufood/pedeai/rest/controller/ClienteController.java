96b8c525bda936471d23cfdb539dd04482383614
Matheus Pozett <maa_souza11@hotmail.com>
2026-07-05 11:00:17 -0300

    feat: adiciona endpoint para retornar historico de pedidos de um cliente

--- src/main/java/br/com/ajufood/pedeai/rest/controller/ClienteController.java
+++ src/main/java/br/com/ajufood/pedeai/rest/controller/ClienteController.java
@@ -2,11 +2,14 @@ package br.com.ajufood.pedeai.rest.controller;
 
 import br.com.ajufood.pedeai.rest.dto.request.ClienteRequestDTO;
 import br.com.ajufood.pedeai.rest.dto.response.ClienteResponseDTO;
+import br.com.ajufood.pedeai.rest.dto.response.PedidoResumoDTO;
 import br.com.ajufood.pedeai.service.ClienteService;
+import br.com.ajufood.pedeai.service.PedidoService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +22,7 @@ import java.util.List;
 public class ClienteController {
 
     private final ClienteService clienteService;
+    private final PedidoService pedidoService;
 
     @Operation(summary = "Busca um cliente pelo id")
     @GetMapping("/{id}")
@@ -57,5 +61,16 @@ public class ClienteController {
     public ResponseEntity<Void> delete(@PathVariable Integer id) {
         clienteService.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @Operation(summary = "Retorna o histórico de pedidos de um cliente")
+    @GetMapping("/{clienteId}/pedidos")
+    public ResponseEntity<Page<PedidoResumoDTO>> getHistoricoPedidos(
+      @PathVariable int clienteId,
+      @RequestParam(required = false) String status,
+      @RequestParam(defaultValue = "0") int pagina,
+      @RequestParam(defaultValue = "10") int tamanho) {
+        Page<PedidoResumoDTO> historico = pedidoService.findHistory(clienteId, status, pagina, tamanho);
+        return ResponseEntity.ok(historico);
     }
 }
